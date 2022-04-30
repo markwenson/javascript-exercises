e3b445b8cf864eb0675dadad89608cb8645337ea
markwenson <markw.lauron@gmail.com>
2022-05-01 02:37:38 +0800

    Edit for answers

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    let string1 = str.toLowerCase().replace(/[&\/\\#, +()$~%.'":*?!<>{}]/g,'');
+    
+    let string2 = "";
 
+    for(let i = string1.length - 1; i >= 0; i--) {
+      string2 = string2 + string1[i];
+    }
+    
+    if (string1 === string2) {
+      return true;
+    } else {
+      return false;
+    }
 };
 
+palindromes("ZZZZ car, a man, a maracaz.");
 // Do not edit below this line
 module.exports = palindromes;
