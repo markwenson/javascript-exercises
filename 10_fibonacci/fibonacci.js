5b6561cfba754c58be10dceb65f1926854482391
markwenson <markw.lauron@gmail.com>
2022-05-02 01:15:21 +0800

    Edit for solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    let number = Number(arg);
+    const fibArray = [0,1];
+    let sequence = 0;
 
+    if (number<0) {
+        return "OOPS";
+    }
+    for (let i = 0; i < number; i++) {
+        sequence = fibArray[i] + fibArray[i+1];
+        fibArray.push(sequence);
+    }
+    return fibArray[number];
 };
+fibonacci("1");
+fibonacci("2");
+fibonacci("8");
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+];
+const getTheTitles = function(arg) {
+      let newArr = arg.map(item => item.title);
+      return newArr;
 };
+getTheTitles(books);
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
