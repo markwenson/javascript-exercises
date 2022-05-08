820778d7561525a428980e0f8a94f616921a0a9c
markwenson <markw.lauron@gmail.com>
2022-05-09 00:59:26 +0800

    Edit for Solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,53 @@
-const findTheOldest = function() {
-
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+const findTheOldest = function(array) {
+  const reduced = array.reduce(reducer);
+  return reduced;
 };
 
+function reducer(oldestPerson, currentPerson) {
+   
+  let oldBirth = oldestPerson.yearOfBirth;
+  let oldDeath = oldestPerson.yearOfDeath;
+  if (!oldDeath) {
+     oldDeath = new Date().getFullYear();
+ 	}; 
+  let currentBirth = currentPerson.yearOfBirth; 
+  let currentDeath = currentPerson.yearOfDeath;
+  if (!currentDeath) {
+     currentDeath = new Date().getFullYear();
+  };
+    
+  const oldestAge = oldDeath - oldBirth;
+  const currentAge = currentDeath - currentBirth;
+
+ 
+  if (oldestAge > currentAge) {
+    return oldestPerson;
+  } else {
+    return currentPerson;
+  }
+};
+
+
+findTheOldest(people);
+
+
+findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,39 @@
-const caesar = function() {
-
+function caesar(string, shift) {
+  const array = string.split("");
+  const array1 = array.map(letter => shiftLetter(letter, shift));
+  return array1.join("");
 };
+
+function letterSet(uCode) {
+  if (uCode < 97) {
+    return 65;
+  } else {
+    return 97;
+  }  
+}
+
+function shiftLetter(letter, shift) {
+
+  const uCode = letter.charCodeAt();
+
+  if ((uCode >= 65 && uCode <= 90) || (uCode >= 97 && uCode <=122)) {
+    return String.fromCharCode(mod((uCode + shift - letterSet(uCode)), 26) + letterSet(uCode));
+  }
+  return letter;
+}
+
+function mod(n, m) {
+  return (n % m + m) % m;
+};
+
+caesar("Hello World", -5)
+caesar("A",1);
+caesar('Aaa', 1);
+caesar('Hello, World!', 5);
+caesar('Mjqqt, Btwqi!', -5);
+caesar('Z', 1);
+caesar('Hello, World!', 75);
+caesar('Hello, World!', -29);
 
 // Do not edit below this line
 module.exports = caesar;
