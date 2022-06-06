f6d73950beecdc69a5a3e665c0750a4edca1588b
markwenson <markw.lauron@gmail.com>
2022-06-06 23:11:41 +0800

    revisit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,20 +1,22 @@
 const palindromes = function (str) {
     
-    let string1 = str.toLowerCase().replace(/[&\/\\#, +()$~%.'":*?!<>{}]/g,'');
-    
-    let string2 = "";
+    let string1 = str.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    let palindrome = "";
 
-    for(let i = string1.length - 1; i >= 0; i--) {
-      string2 = string2 + string1[i];
+    for(let i = string1.length-1; i >=0; i--) {
+      palindrome = palindrome + string1[i]
     }
     
-    if (string1 === string2) {
+    if(string1 === palindrome) {
       return true;
     } else {
       return false;
     }
+ 
 };
 
-palindromes("ZZZZ car, a man, a maracaz.");
+
+
+
 // Do not edit below this line
 module.exports = palindromes;
